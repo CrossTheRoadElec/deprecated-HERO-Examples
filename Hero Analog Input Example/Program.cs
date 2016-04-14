92f96022358be21b3d7307ab27e0ef662a982bef
Jacob Caporuscio <jcaporuscio@ctr-electronics.com>
2016-04-14 12:31:13 -0400

    Created Peripheral Library CTRE.HERO.IO
    Analog/Serial Examples now utilize new peripheral library.  Library will
    be included in next SDK revision.  Until then, examples will include
    HERO.IO.cs as needed.

--- Hero Analog Input Example/Program.cs
+++ Hero Analog Input Example/Program.cs
@@ -2,15 +2,17 @@
 using System.Threading;
 using Microsoft.SPOT;
 using Microsoft.SPOT.Hardware;
+using CTRE.HERO.IO;
+
 
 namespace $safeprojectname$
 {
     public class Program
     {
         /* create analog inputs from PORT1(AUSX) */
-        static AnalogInput analogInput0 = new AnalogInput(Microsoft.SPOT.Hardware.Cpu.AnalogChannel.ANALOG_0);
-        static AnalogInput analogInput1 = new AnalogInput(Microsoft.SPOT.Hardware.Cpu.AnalogChannel.ANALOG_1);
-        static AnalogInput analogInput2 = new AnalogInput(Microsoft.SPOT.Hardware.Cpu.AnalogChannel.ANALOG_2);
+        static AnalogInput analogInput0 = new AnalogInput(Port1.Analog_Pin3);
+        static AnalogInput analogInput1 = new AnalogInput(Port1.Analog_Pin4);
+        static AnalogInput analogInput2 = new AnalogInput(Port1.Analog_Pin5);
 
         public static void Main()
         {
