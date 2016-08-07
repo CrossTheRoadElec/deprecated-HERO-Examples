0bf226a988973ea8c60e61250367634d4f0e6762
Omar Zrien <OZrien@crosstheroadelectronics.com>
2016-08-06 20:35:25 -0400

    Added NeoPixel example using SPI port.

---
+++ HERO NeoPixel Example/HeroPixel.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.SPOT.Hardware;
+
+/// <summary>
+/// Updates a series of NeoPixels.
+/// Call setColor (or setStripColor) to update individual pixels colors (or all at once).
+/// Call writeOutput to flush latest LED values.  
+/// </summary>
+public class HeroPixel
+{
+    // const values for inflated bits
+    private const byte T0 = 0xC0; // 1100 0000 (9MHz SPI transmission translates to a 0 bit)
+    private const byte T1 = 0xFe; // 1111 1110 (9MHz SPI transmission translates to a 1 bit)
+    private const byte START_OF_RST = 0x00; // 0000 0000 (used to add rest between transmissions)
+
+    // const values for common colors
+    public const uint WHITE = 0xFFFFFF;
+    public const uint OFF = 0x000000;
+    public const uint RED = 0xFF0000;
+    public const uint GREEN = 0x00FF00;
+    public const uint BLUE = 0x0000FF;
+    public const uint CYAN = 0x00FFFF;
+    public const uint MAGENTA = 0xFF00FF;
+    public const uint YELLOW = 0xFFFF00;
+    public const uint PURPLE = 0x800080;
+    public const uint ORANGE = 0xFF3a00;
+    public const uint PINK = 0xFF6065;
+
+    // used for SPI communication
+    SPI SPIDevice;
+    SPI.Configuration Configuration;
+
+    // instance variables
+    uint[] _pixels;         // RGB values of each pixel
+    byte[] _spiOut;
+    uint _numPixels;         // strip's length
+
+    // constructor
+    public HeroPixel(uint color, uint numPixels = 1, SPI.SPI_module spiModule = SPI.SPI_module.SPI4) 
+    {
+        _numPixels = (numPixels > 0) ? numPixels : 1;
+
+        _pixels = new uint[_numPixels];
+        _spiOut = new byte[3 * 8 * _numPixels + 1]; // create array for output
+
+        setStripColor(color);
+
+        // initialize SPI
+        Configuration = new SPI.Configuration(Cpu.Pin.GPIO_NONE, false, 0, 0, false, false, 9000, spiModule);
+        SPIDevice = new SPI(Configuration);
+    }
+    // sets color values for pixels strip of size length beginning at start
+    public void setColor(uint color, uint start, uint length)
+    {
+        for (uint i = start; i < (start + length); i++)
+        {
+            /* only update the SPI bytes if pixel has changed */
+            if (_pixels[i] != color)
+            {
+                _pixels[i] = color;
+
+                /* update spi map */
+                uint counter = 3 * 8 * i;                         // keeps track of position in array
+                byte grn = (byte)(color >> 8);
+                byte red = (byte)(color >> 16);
+                byte blu = (byte)(color >> 0);
+                // inflate every bit in green byte and add to output array, starting with most significant
+                for (byte j = 0x80; j > 0; j >>= 1)
+                {
+                    if ((grn & j) != 0) { _spiOut[counter++] = T1; }
+                    else { _spiOut[counter++] = T0; }
+                }
+
+                // inflate every bit in red byte and add to output array, starting with most significant
+                for (byte j = 0x80; j > 0; j >>= 1)
+                {
+                    if ((red & j) != 0) { _spiOut[counter++] = T1; }
+                    else { _spiOut[counter++] = T0; }
+                }
+
+                // inflate every bit in blue byte and add to output array, starting with most significant
+                for (byte j = 0x80; j > 0; j >>= 1)
+                {
+                    if ((blu & j) != 0) { _spiOut[counter++] = T1; }
+                    else { _spiOut[counter++] = T0; }
+                }
+            }
+        }
+    }
+
+    // sets color of strip
+    public void setStripColor(uint color)
+    {
+        setColor(color, 0, _numPixels);
+    }
+    // outputs the data over 9 MHz SPI transmission
+    public void writeOutput()
+    {
+        SPIDevice.Write(_spiOut);
+    }
+    public uint NumberPixels
+    {
+        get
+        {
+            return _numPixels;
+        }
+    }
+}
---
+++ HERO NeoPixel Example/Program.cs
@@ -0,0 +1,120 @@
+﻿/// <summary>
+/// Example project controlling NeoPixels through any SPI (S) port.
+/// </summary>
+using System;
+using System.Threading;
+
+namespace NeoPixel_Example
+{
+    public class Program
+    {
+        const uint kNumOfNeoPixels = 30;
+
+        /* lets make some NeoPixels, and default the color */
+        HeroPixel _heroPixels = new HeroPixel(HeroPixel.OFF, kNumOfNeoPixels);
+
+        uint[] _colorSequence =
+        {
+            HeroPixel.WHITE,
+            HeroPixel.PINK,
+            HeroPixel.CYAN,
+            HeroPixel.GREEN,
+            HeroPixel.MAGENTA,
+            HeroPixel.ORANGE,
+            HeroPixel.PURPLE,
+            HeroPixel.RED,
+            HeroPixel.YELLOW,
+            HeroPixel.BLUE,
+        };
+        /// <summary> Color index in color sequence.</summary>
+        uint _colIdx = 0;
+
+        /// <summary> Pixel index for when using the color sequence.  </summary>
+        uint _pixelIdx = 0;
+
+        /// <summary> Tracks button events.  </summary>
+        bool _lastBtn = false;
+
+        /// <summary> TRUE iff color is controlled by user interface, FALSE if streaming through color sequence. </summary>
+        bool _colorWheelMode = false;
+
+        public void RunForever()
+        {
+            /* get an X,Y, and btn value.  These could come from potentiometers for example, or USB gamepad */
+            Microsoft.SPOT.Hardware.AnalogInput aiBtn = new Microsoft.SPOT.Hardware.AnalogInput(CTRE.HERO.IO.Port1.Analog_Pin3);
+            Microsoft.SPOT.Hardware.AnalogInput aiX = new Microsoft.SPOT.Hardware.AnalogInput(CTRE.HERO.IO.Port1.Analog_Pin4);
+            Microsoft.SPOT.Hardware.AnalogInput aiY = new Microsoft.SPOT.Hardware.AnalogInput(CTRE.HERO.IO.Port1.Analog_Pin5);
+
+            /* loop forever */
+            while (true)
+            {
+                /* loop sleep */
+                Thread.Sleep(5);
+
+                /* get x,y, and button */
+                bool btn = aiBtn.Read() < 0.5f;
+                float x = (float)aiX.Read() * 2f - 1f; // [-1,1]
+                float y = (float)aiY.Read() * 2f - 1f; // [-1,1]
+
+                /* figure out next color based on current mode */
+                if (_colorWheelMode)
+                {
+                    /* spin around the top circular slice of an HSV surface,
+                    https://en.wikipedia.org/wiki/HSL_and_HSV#HSV */
+
+                    /* calc sat and hue, use 100% for value */
+                    float hueDeg = (float)System.Math.Atan2(y, x) * 180f / (float)System.Math.PI;
+                    float sat = (float)System.Math.Sqrt(x * x + y * y);
+                    if (hueDeg < 0) { hueDeg += 360f; }
+
+                    /* convert to rgb */
+                    uint color;
+                    uint r, g, b;
+                    HsvToRgb.Convert(hueDeg, sat, 1, out r, out g, out b);
+                    color = r;
+                    color <<= 8;
+                    color |= g;
+                    color <<= 8;
+                    color |= b;
+
+                    /* set all LEDs to one color, controlled by analog signals */
+                    _heroPixels.setStripColor(color);
+                }
+                else
+                {
+                    /* just ramp through the predetermined color sequence */
+                    uint color = _colorSequence[_colIdx];
+                    _heroPixels.setColor(color, _pixelIdx, 1);
+                }
+
+                /* update LEDs */
+                _heroPixels.writeOutput();
+                _heroPixels.writeOutput();
+
+                /* iterate to next pixel */
+                if (++_pixelIdx >= _heroPixels.NumberPixels)
+                {
+                    /* back to first pixel */
+                    _pixelIdx = 0;
+
+                    /* step to next color when using the predetermined sequence */
+                    if (++_colIdx >= _colorSequence.Length) { _colIdx = 0; }
+                }
+
+                /* detect on-press event on button to change mode. */
+                if(btn && !_lastBtn)
+                {
+                    _colorWheelMode = !_colorWheelMode;
+                }
+                _lastBtn = btn;
+            }
+        }
+        /// <summary>
+        /// Entry point into program.
+        /// </summary>
+        public static void Main()
+        {
+            new Program().RunForever();
+        }
+    }
+}
